f2c66334b7a1992b29eacb5b2aaf6e1a4935cebc
drkwitht <derektan3510@gmail.com>
2023-06-12 20:51:05 -0700

    Prewrote resource utils.

---
+++ src/utils/rescache.py
@@ -0,0 +1,25 @@
+"""
+    @file rescache.py
+    @author Derek Tan
+"""
+
+import os
+import utils.resources as resources
+
+class ResourceCache:
+    def __init__(self, public_dirname: str):
+        self.indexes = {}  # NOTE: maps file names to resource objects.
+
+        dir_entry = os.scandir(public_dirname)
+
+        for item in dir_entry:
+            if item.is_file():
+                self.add_item(item.name, resources.StaticResource(item.path))
+
+        dir_entry.close()
+    
+    def add_item(self, file_name: str, res_obj: resources.StaticResource):
+        self.indexes[file_name] = res_obj
+
+    def get_item(self, file_name: str):
+        return self.indexes[file_name]
---
+++ src/utils/resources.py
@@ -0,0 +1,55 @@
+"""
+    @file resources.py
+    @author Derek Tan
+"""
+
+FS_SEEK_START = 0
+FS_SEEK_END = 2
+MIME_TYPE_TEXT = "text/plain"
+MIME_TYPE_HTML = "text/html"
+MIME_TYPE_CSS = "text/css"
+MIME_TYPE_ANY = "*/*"  # NOTE: assume MIME any type as raw binary!
+
+FILE_EXTS_TO_MIME = {
+    "txt": MIME_TYPE_TEXT,
+    "html": MIME_TYPE_HTML,
+    "css": MIME_TYPE_CSS,
+    "foo": MIME_TYPE_ANY
+}
+
+class StaticResource:
+    def __init__(self, file_path: str):
+        self.type = MIME_TYPE_ANY
+        self.data = None
+        self.length = 0
+
+        dot_pos = file_path.find(".", 1)
+        file_ext = "foo"
+
+        if dot_pos > 0:
+            file_ext = file_path[dot_pos + 1 : ]
+            self.type = FILE_EXTS_TO_MIME[file_ext]
+        else:
+            self.type = FILE_EXTS_TO_MIME["foo"]
+
+        file_stream = open(file_path, "rb")
+        file_stream.seek(0, FS_SEEK_END)
+        file_length = file_stream.tell()
+        file_stream.seek(0, FS_SEEK_START)
+
+        self.data = file_stream.read(file_length)
+        self.length = file_length
+
+        file_stream.close()
+
+    def get_mime_type(self):
+        return self.type
+    
+    def get_content_len(self):
+        return self.length
+
+    def as_bytes(self):
+        return self.data
+    
+    def as_text(self):
+        return self.data.decode(encoding="ascii")
